7bba0ab0ec77c9ca6436a373387b2dd081337b40
sankaraxi <sankarfrompalani@gmail.com>
2024-09-14 23:22:39 +0530

    added hero section

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+const Button = ({label,iconURL}) => {
+    return (
+        <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+            {label}
+
+            <img
+                src = {iconURL}
+                alt="icon"
+                className="w-5 h-5 ml-2 rounded-full"
+            />
+        </button>
+    );
+}
+
+export default Button;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,10 @@
 import { headerLogo } from '../assets/images';
 import { hamburger} from '../assets/icons';
+import { navLinks } from '../constants';
 
 const Nav = () => {
   return (
-    <header className='padding-x py-8 absoulte z-10 w-full'>
+    <header className='padding-x py-8 absolute z-10 w-full'>
         <nav className='flex justify-between items-center max-container'>
             <a href='/'>
                 <img 
@@ -13,6 +14,30 @@ const Nav = () => {
                     height={29}
                 />
             </a>
+
+            <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden '>
+              {
+                navLinks.map((item) => (
+                  <li key={item.label}>
+                    <a 
+                      href={item.href}
+                      className='font-montserrat leading-normal text-lg text-slate-grey '
+                    >
+                      {item.label}
+                    </a>
+                  </li>
+                ))
+              }
+            </ul>
+
+            <div className='hidden max-lg:block'>
+              <img 
+                src={hamburger}
+                alt='hamburger'
+                width={24}
+                height={24}
+              />
+            </div>
 
         </nav>
     </header>
